185ab17120b78a8eb93f1e2b60828e92fc61205d
Elif <elifnur.telase@gmail.com>
2022-01-16 01:17:50 +0200

    Algoritma sorusu tamamlandı.

---
+++ Algoritma/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        letter(sc.nextLine());
+        sc.close();
+    }
+
+    static void letter(String str)
+    {   
+        String newStr = str.replaceAll("[^A-Za-z]", "");
+
+        System.out.println(newStr);
+
+    }
+}
