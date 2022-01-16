ab801cc871af9c9fb6ef0f2bbfc932f585e79015
Elif <elifnur.telase@gmail.com>
2022-01-16 16:22:57 +0200

    Mutlak deger alma tamamlandi

---
+++ Orta/MutlakKareAlma/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MutlakKareAlma
+{
+    class Program
+    {
+        static int k=0,b=0;
+        static void Main(string[] args)
+        {
+            Console.Write("Kaç sayı gireceksiniz:");
+            int n= Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                System.Console.Write("Sayi gir:");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+
+                if(sayi>67)
+                    b += (int)Math.Pow((sayi-67),2);
+                else
+                    k+= 67-sayi;
+            }
+
+            System.Console.WriteLine(k+" , "+b);
+        }
+    }
+}
