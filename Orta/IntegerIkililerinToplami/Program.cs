03f943555a08561618a16f775eeb7d388aa01ce0
Elif <elifnur.telase@gmail.com>
2022-01-16 15:24:13 +0200

    Ikililerin toplami tamamlandi.

---
+++ Orta/IntegerIkililerinToplami/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace IntegerIkililerinToplami
+{
+    class Program
+    {
+        static int n, m = 0;
+
+        static void Main(string[] args)
+        {
+
+            System.Console.Write("Kaç sayi gireceksiniz (Çift girin) :");
+            n = Convert.ToInt16(Console.ReadLine());
+            int[] arr = new int[(n / 2) + 1];
+
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                System.Console.Write("Sayi gir:");
+                int x = Convert.ToInt16(Console.ReadLine());
+                System.Console.Write("Sayi gir:");
+                int y = Convert.ToInt16(Console.ReadLine());
+                arr[i] = deneme(x, y);
+            }
+            if (n % 2 != 0)
+            {
+                System.Console.Write("Sayi gir:");
+                arr[n / 2] = Convert.ToInt16(Console.ReadLine());
+            }
+
+            foreach (int item in arr)
+            {
+                if(item != 0)
+                System.Console.Write(item + " ");
+            }
+
+
+            static int deneme(int k, int y)
+            {
+                if (k == y)
+                    return (k + k) * (k + k);
+                else
+                    return k + y;
+            }
+        }
+    }
+}
