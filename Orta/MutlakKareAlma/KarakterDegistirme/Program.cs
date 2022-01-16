cf759b7aa4167eff5070316f066adb760fb89d78
Elif <elifnur.telase@gmail.com>
2022-01-16 19:39:14 +0200

    Karakter degistirme tamamlandi.

---
+++ Orta/MutlakKareAlma/KarakterDegistirme/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace KarakterDegistirme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir cumle girin:");
+            string str = Console.ReadLine();
+
+            string[] strArr = str.Split(" ");
+            string[] strArrReverese = new string[strArr.Length];
+            
+            
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                char[] reverse = strArr[i].ToCharArray();
+                Array.Reverse(reverse);
+                strArrReverese[i] =  new string(reverse);
+
+            }
+
+            foreach (string item in strArrReverese)
+            {
+                System.Console.Write(item+" ");
+            }
+        }
+    }
+}
